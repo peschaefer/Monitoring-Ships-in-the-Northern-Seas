6450be6c3ffe536cfa4b6ad49033a32aee26ad3f
DrewH <adheiss@bsu.edu>
2022-11-21 19:20:23 -0500

    add Static Data storing for AIS Message

--- src/controllers/ais-message.controllers.js
+++ src/controllers/ais-message.controllers.js
@@ -16,32 +16,54 @@ const store = async (request, response) => {
     //     response.status(400).json({status: valid})
     //     return
     // }
-    console.log(aisMessage.Position.coordinates[0])
-
+    //figure out timestamp datetime format to insert, no value sent right now
     if (aisMessage.MsgType === "position_report") {
-        const result = await db.query(`INSERT INTO AIS_MESSAGE (MMSI, Class)
-                                       VALUES ('${aisMessage.MMSI}', '${aisMessage.Class}')`)
+        await storePositionReport(aisMessage)
 
-        const positionResult = await db.query(`INSERT INTO POSITION_REPORT (AISMessage_Id, NavigationalStatus,
-                                                                            Longitude, Latitude, RoT, SoG, CoG, Heading)
-                                               VALUES ('${result.insertId}', '${aisMessage.Status}',
-                                                       '${aisMessage.Position.coordinates[0]}',
-                                                       '${aisMessage.Position.coordinates[1]}',
-                                                       '${aisMessage.RoT}', '${aisMessage.SoG}',
-                                                       '${aisMessage.CoG}', '${aisMessage.Heading}')`)
-
-        if(positionResult.affectedRows) {
-            response.status(201).json({valid: true, data: request.body})
-        }
-        else {
-            response.status(400).json({valid: false, message: positionResult})
-        }
     } else if (aisMessage.MsgType === "static_data") {
-
+        await storeStaticData(aisMessage)
     }
 
+}
+//move to own controller?
+const storePositionReport = async (aisMessage) => {
+    const result = await db.query(`INSERT INTO AIS_MESSAGE (MMSI, Class)
+                                   VALUES ('${aisMessage.MMSI}', '${aisMessage.Class}')`)
 
+    const positionResult = await db.query(`INSERT INTO POSITION_REPORT (AISMessage_Id, NavigationalStatus,
+                                                                        Longitude, Latitude, RoT, SoG, CoG, Heading)
+                                           VALUES ('${result.insertId}', '${aisMessage.Status}',
+                                                   '${aisMessage.Position.coordinates[0]}',
+                                                   '${aisMessage.Position.coordinates[1]}',
+                                                   '${aisMessage.RoT}', '${aisMessage.SoG}',
+                                                   '${aisMessage.CoG}', '${aisMessage.Heading}')`)
 
+    if (positionResult.affectedRows) {
+        response.status(201).json({valid: true, data: request.body})
+    } else {
+        response.status(400).json({valid: false, message: positionResult})
+    }
+}
+//move to own controller?
+const storeStaticData = async (aisMessage) => {
+    const result = await db.query(`INSERT INTO AIS_MESSAGE (MMSI, Class)
+                                   VALUES ('${aisMessage.MMSI}', '${aisMessage.Class}')`)
+    //eta and port id, cross query later
+    const staticResult = await db.query(`INSERT INTO STATIC_DATA (AISMessage_Id, AISIMO, CallSign, Name, VesselType,
+                                                                  CargoType, Length, Breadth, Draught,
+                                                                  AISDestination)
+                                         VALUES ('${result.insertId}', '${aisMessage.IMO}',
+                                                 '${aisMessage.CallSign}',
+                                                 '${aisMessage.Name}',
+                                                 '${aisMessage.VesselType}', '${aisMessage.CargoType}',
+                                                 '${aisMessage.Length}', '${aisMessage.Breadth}',
+                                                 '${aisMessage.Draught}', '${aisMessage.Destination}')`)
+
+    if (staticResult.affectedRows) {
+        response.status(201).json({valid: true, data: request.body})
+    } else {
+        response.status(400).json({valid: false, message: staticResult})
+    }
 }
 
 module.exports = {
