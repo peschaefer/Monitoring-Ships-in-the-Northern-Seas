94576e10c9cc8fcadc9266ed93d98e5e0670b438
Peyton Schaefer <peschaefer@bsu.edu>
2022-12-05 13:48:58 -0500

    Improve getPort route with optional country entry

--- src/controllers/port.controllers.js
+++ src/controllers/port.controllers.js
@@ -6,8 +6,20 @@ const getPorts = async (request, response) => {
 }
 
 const getPort = async (request, response) => {
+    //http://localhost:8081/port?port=${port_name}
     const port = request.query.port
-    const data = await db.query(`SELECT * FROM port WHERE Name='${port}'`)
+    const country = request.query.country
+    let data
+    if(country) {
+        data = await db.query(`SELECT *
+                                     FROM port
+                                     WHERE Name = '${port}'
+                                       AND Country = '${country}'`)
+    } else{
+        data = await db.query(`SELECT *
+                                     FROM port
+                                     WHERE Name = '${port}'`)
+    }
     response.status(200).json({data})
 }
 
