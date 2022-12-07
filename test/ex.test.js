38475835121560f800ef054e4ffb9e4aad790e7e
DrewH <adheiss@bsu.edu>
2022-12-06 22:15:51 -0500

    add comments for things that will need to be tested

--- test/ex.test.js
+++ test/ex.test.js
@@ -7,3 +7,27 @@ describe("Test Getting A Vessel", () => {
         expect(response.body.data[0].IMO).toBe(1000019)
     })
 })
+
+//insert full valid AIS batch
+
+//insert one invalid AIS Message in batch
+
+//insert AIS Message, position report with no batch
+
+//insert AIS Message, static data with no batch
+
+//Test delete ais route
+
+//get most recent position from mmsi
+
+//test getting port by name
+
+//test getting port by country?
+
+//last 5 mmsi positions
+
+//test get/insert queries with db.js
+
+//sql injection?
+
+//do we want a testing mode flag for functions to not query the database whatsoever?
